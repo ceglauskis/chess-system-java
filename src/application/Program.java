af1ae5926e63da665895178afd68c3446b3043bd
maikoceglauskis@gmail.com <maikoceglauskis@gmail.com>
2026-01-07 19:15:47 -0300

    Checkmate logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,7 +18,7 @@ public class Program {
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
 
-        while (true) {
+        while (!chessMatch.getCheckMate()) {
             try {
 
                 UI.clearScreen();
@@ -44,5 +44,7 @@ public class Program {
                 sc.nextLine();
             }
         }
+        UI.clearScreen();
+        UI.printMatch(chessMatch, captured);
     }
 }
