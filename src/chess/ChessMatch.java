d08e64e57c772d084f48ffab9e8f16ba70f6484e
maikoceglauskis@gmail.com <maikoceglauskis@gmail.com>
2026-01-08 17:24:52 -0300

    Fix Promotion Exception

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -193,10 +193,10 @@ public class ChessMatch {
 
     public ChessPiece replacePromotedPiece(String type){
         if (promoted == null){
-            return promoted;
+            throw new IllegalStateException("There is no piece to be promoted");
         }
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")){
-            throw new InvalidParameterException("Invalid type for promotion");
+            return promoted;
         }
 
         Position pos = promoted.getChessPosition().toPosition();
